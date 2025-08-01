0382cb3dbec9f43e734177a7b16655c910095875
syuhao666 <aadsmouse@gmail.com>
2025-08-01 09:04:18 +0800

    080101

--- ordersystem/src/main/java/tw/syuhao/ordersystem/controller/ProductController.java
+++ ordersystem/src/main/java/tw/syuhao/ordersystem/controller/ProductController.java
@@ -2,33 +2,24 @@ package tw.syuhao.ordersystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tw.syuhao.ordersystem.entity.Product;
-import tw.syuhao.ordersystem.repository.ProductRepository;
+import tw.syuhao.ordersystem.service.ProductService;
 
-@Controller
-@RequestMapping("/admin/products")
-public class ProductController {
+// @Controller
+// @RequestMapping("/admin/products")
+// public class ProductController {
 
-    @Autowired
-    private ProductRepository productRepository;
+//     @Autowired
+//     private ProductService productService;
 
-    // 顯示新增表單
-    @GetMapping("/add")
-    public String showAddForm(Model model) {
-        model.addAttribute("product", new Product());
-        return "add-product"; // 對應 add-product.html
-    }
-
-    // 處理表單提交
-    @PostMapping("/add")
-    public String addProduct(@ModelAttribute Product product) {
-        productRepository.save(product);
-        return "redirect:/admin/products"; // 返回商品列表
-    }
-}
+//     @PostMapping("/admin/products/add")
+//     public String addProduct(@ModelAttribute Product product) {
+//         productService.save(product);
+//         return "redirect:/admin/products";
+//     }
+    
+// }
