42791e5f0e7e70a991d5729755fba6af95f0bbf0
syuhao666 <aadsmouse@gmail.com>
2025-08-11 08:50:10 +0800

    081101

--- spring/src/main/java/tw/dd/spring/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package tw.dd.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.dd.spring.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
