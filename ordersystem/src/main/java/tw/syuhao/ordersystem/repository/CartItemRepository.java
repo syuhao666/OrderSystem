c4bf104e99809629cd228873b6cd671e5866ecfd
ddvgh <ms0590341@gmail.com>
2025-08-12 12:39:27 +0800

    123

--- ordersystem/src/main/java/tw/syuhao/ordersystem/repository/CartItemRepository.java
+++ ordersystem/src/main/java/tw/syuhao/ordersystem/repository/CartItemRepository.java
@@ -3,6 +3,8 @@ package tw.syuhao.ordersystem.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import tw.syuhao.ordersystem.entity.Cart;
 import tw.syuhao.ordersystem.entity.CartItem;
@@ -12,5 +14,9 @@ import tw.syuhao.ordersystem.entity.CartItem;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     
     List<CartItem> findByCart(Cart cart);
+
+    // @Transactional
+    @Modifying
+    @Query("delete from CartItem ci where ci.cart = :cart")
     void deleteByCart(Cart cart);
 }
