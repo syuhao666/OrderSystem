42571236aaa7c62b3173e080533cd7628f59e743
syuhao666 <aadsmouse@gmail.com>
2025-08-13 14:26:53 +0800

    訂單管理分頁

--- ordersystem/src/main/java/tw/syuhao/ordersystem/service/OrderService.java
+++ ordersystem/src/main/java/tw/syuhao/ordersystem/service/OrderService.java
@@ -1,9 +1,12 @@
 package tw.syuhao.ordersystem.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import tw.syuhao.ordersystem.entity.Order;
@@ -30,5 +33,9 @@ public class OrderService {
     public void delete(Integer id) {
         orderRepository.deleteById(id);
     }
-}
 
+    public Page<Order> getOrders(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        return orderRepository.findAll(pageable);
+    }
+}
